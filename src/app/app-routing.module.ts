9daa2cd9f8e7f89f9bb54fa7083a1480a07d3a6e
DylanKeane <43648552+DylanKeane@users.noreply.github.com>
2019-11-22 15:45:36 +0000

    Dylan animated splash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   { path: 'shoppinglist', loadChildren: './shoppinglist/shoppinglist.module#ShoppinglistPageModule' },
   { path: 'bills', loadChildren: './bills/bills.module#BillsPageModule' },
   { path: 'sidebar', loadChildren: './sidebar/sidebar.module#SidebarPageModule' },
-  { path: 'sidebar:id', loadChildren: './sidebar/sidebar.module#SidebarPageModule' },
+  { path: 'sidebar:id', loadChildren: './sidebar/sidebar.module#SidebarPageModule' },  { path: 'todolist', loadChildren: './todolist/todolist.module#TodolistPageModule' },
+
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,32 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { timer } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
+ templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
   constructor(
-    private platform: Platform,
+   private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+   private statusBar: StatusBar,
+    
   ) {
     this.initializeApp();
   }
+
+  showSplash = true; // <-- show animation
+
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      timer(5000).subscribe(() => this.showSplash = false) // <-- hide animation after 3s
     });
   }
 }
